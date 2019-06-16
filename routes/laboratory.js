043f100f386ff40d4094aaf7597f8d4621cfc4e5
jeffersoninacio@hotmail.com <jeffersoninacio@hotmail.com>
2019-06-16 19:09:55 -0300

    Cadastro de laboratorio
    O cadastrado de um novo laboratório está pronto, o usuário não consegue cadastrar dois laboratórios com o mesmo nome e endereço, mas é possível o cadastro com endereços diferentes. Caso tente cadastrar um laboratório com o mesmo nome e endereço, a página é redirecionda para a view que lista todos os laboratórios e uma mensagem de erro é renderizada.

--- routes/laboratory.js
+++ routes/laboratory.js
@@ -2,12 +2,57 @@ const express = require('express');
 const router = express.Router();
 const Laboratory = require('../models/Laboratory.js');
 
+/* Register a new laboratory */
+router.get('/new-lab', (req, res, next) => {
+  res.render('../views/laboratory/new-lab');
+});
+
+router.post('/new-lab', (req, res, next) => {
+  const {
+    name,
+    address,
+    status
+  } = req.body;
+
+  Laboratory.findOne({address})
+    .then(( register ) => {         
+      if( register !== null) {
+        res.redirect('/laboratories/list-lab/?msg=Este laboratório e o endereço já existem.');  
+        return;        
+      }
+      else{     
+        const newLaboratory = new Laboratory({
+          name,
+          address,
+          status
+        });
+        newLaboratory.save()
+        .then((laboratory) => {
+          res.redirect('/laboratories/list-lab');
+        })
+        .catch((error) => {
+          console.log(error);
+        });     
+      
+      }
+
+    })
+    .catch( error => {
+      console.log('Erro: ', error);      
+    });
+    
+
+
+  
+});
+
 /* List all laboratories */
 router.get('/list-lab', (req, res, next) => {
   Laboratory.find()
-    .then( listLabs => {
+    .then(listLabs => {
       res.render('../views/laboratory/list-lab', {
-        laboratories: listLabs
+        laboratories: listLabs, 
+        msg: req.query.msg,
       });
     })
     .catch(error => {
@@ -17,22 +62,17 @@ router.get('/list-lab', (req, res, next) => {
 
 /* Laboratory details */
 router.get('/:id', (req, res, next) => {
-  Laboratory.findOne( {'_id': req.params.id} )
-  .then( theLab => {
-    res.render('../views/laboratory/laboratory-details', { laboratory: theLab } );
-  })
-  .catch( error => {
-    console.log("Error while retrieving laboratory detail: ", error); 
-  })
+  Laboratory.findOne({
+      '_id': req.params.id
+    })
+    .then(theLab => {
+      res.render('../views/laboratory/laboratory-details', {
+        laboratory: theLab
+      });
+    })
+    .catch(error => {
+      console.log("Erro ao recuperar detalhes de um laboratório: ", error);
+    })
 });
-
-/* Register a new laboratory */
-router.get('/new-lab', (req, res, next) => {
-  Laboratory.save()
-  .then( newLab => {
-    
-
-  })
-})
 
 module.exports = router;
